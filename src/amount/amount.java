26bb8ae34ab58c2c3fd73ad5f0ab85031a9663ee
Harish.V <harishragavendar003@gmail.com>
2023-12-29 22:16:17 +0530

    tast5

---
+++ src/amount/amount.java
@@ -0,0 +1,23 @@
+package amount;
+import java.util.Scanner;
+public class amount {
+
+	public static void main(String[] args) {
+		 Scanner Scanner = new Scanner(System.in);
+	        System.out.print("Enter the purchase amount: ");
+	        double purchaseAmount = Scanner.nextDouble();
+	        double discount = 0.0;
+
+	        if (purchaseAmount >= 500 && purchaseAmount <= 1000) {
+	            discount = 0.1; // 10% discount
+	        } else if (purchaseAmount > 1000) {
+	            discount = 0.2; // 20% discount
+	        }
+	        double payableAmount = purchaseAmount - (purchaseAmount * discount);
+	        System.out.println("Discount applied: " + (discount * 100) + "%");
+	        System.out.println("Final payable amount: $" + payableAmount);
+	        Scanner.close();
+
+	}
+
+}
